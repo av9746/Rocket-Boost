401121c77fa8db1dc56a306719a2d11d053d41e4
admin <anze.vavken@gmail.com>
2019-05-23 19:08:12 +0200

    Added thrusting sound for the rocket

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,9 +5,13 @@ using UnityEngine.Experimental.PlayerLoop;
 
 public class Rocket : MonoBehaviour {
     private Rigidbody rigidBody;
+    private AudioSource audioSource;
+    
     // Start is called before the first frame update
     void Start() {
         rigidBody = gameObject.GetComponent<Rigidbody>();
+        audioSource = gameObject.GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -19,8 +23,14 @@ public class Rocket : MonoBehaviour {
     private void ProcessInput() {
         if (Input.GetKey(KeyCode.Space)) { //can thrust while rotating
             rigidBody.AddRelativeForce(Vector3.up);
-            print("Thrusting");
-        } 
+            print("thrusting");
+            if (!audioSource.isPlaying) {
+                audioSource.Play();           
+            }
+
+        } else {
+            audioSource.Stop();
+        }
         
         if (Input.GetKey(KeyCode.D)) { // can rotate only one way simuntaniously
             transform.Rotate(Vector3.forward); //forward means z axis
@@ -29,4 +39,6 @@ public class Rocket : MonoBehaviour {
         }
         
     }
+    
+    
 }

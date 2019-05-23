7f2f0c6f72d61eeaf3d70de03de4acf5b32f0a67
admin <anze.vavken@gmail.com>
2019-05-23 07:31:54 +0200

    Rocket placeholder + basic movement

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class Rocket : MonoBehaviour {
+    private Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start() {
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space)) { //can thrust while rotating
+            rigidBody.AddRelativeForce(Vector3.up);
+            print("Thrusting");
+        } 
+        
+        if (Input.GetKey(KeyCode.D)) { // can rotate only one way simuntaniously
+            transform.Rotate(Vector3.forward); //forward means z axis
+        } else if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(-Vector3.forward);
+        }
+        
+    }
+}
